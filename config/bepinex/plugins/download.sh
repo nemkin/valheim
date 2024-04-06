a8b8da8cee181bb78d110ca36e7f243de3a9efc4
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2024-04-06 09:31:34 +0100

    Add

--- config/bepinex/plugins/download.sh
+++ config/bepinex/plugins/download.sh
@@ -5,7 +5,7 @@ wget https://gcdn.thunderstore.io/live/repository/packages/Azumatt-AzuExtendedPl
 wget https://gcdn.thunderstore.io/live/repository/packages/GemHunter1-CustomChestName-1.1.1.zip
 wget https://gcdn.thunderstore.io/live/repository/packages/Nextek-SpeedyPaths-1.0.8.zip
 wget https://gcdn.thunderstore.io/live/repository/packages/clevel-BeastsOfBurden-1.0.4.zip
-wget https://gcdn.thunderstore.io/live/repository/packages/ValheimModding-Jotunn-2.18.1.zip
+wget https://gcdn.thunderstore.io/live/repository/packages/ValheimModding-Jotunn-2.18.2.zip
 wget https://gcdn.thunderstore.io/live/repository/packages/Digitalroot-Heightmap_Unlimited_Remake-1.4.0.zip
 wget https://gcdn.thunderstore.io/live/repository/packages/JereKuusela-Vanity-1.8.0.zip
 wget https://gcdn.thunderstore.io/live/repository/packages/Azumatt-AzuAutoStore-2.1.6.zip
