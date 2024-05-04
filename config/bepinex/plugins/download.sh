eb30207547cbb128311ef1786a18f7ac5d79778a
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2024-05-04 11:44:14 +0100

    Add

--- config/bepinex/plugins/download.sh
+++ config/bepinex/plugins/download.sh
@@ -5,11 +5,11 @@ wget https://gcdn.thunderstore.io/live/repository/packages/Azumatt-AzuExtendedPl
 wget https://gcdn.thunderstore.io/live/repository/packages/GemHunter1-CustomChestName-1.1.1.zip
 wget https://gcdn.thunderstore.io/live/repository/packages/Nextek-SpeedyPaths-1.0.8.zip
 wget https://gcdn.thunderstore.io/live/repository/packages/clevel-BeastsOfBurden-1.0.4.zip
-wget https://gcdn.thunderstore.io/live/repository/packages/ValheimModding-Jotunn-2.18.2.zip
+wget https://gcdn.thunderstore.io/live/repository/packages/ValheimModding-Jotunn-2.19.3.zip
 wget https://gcdn.thunderstore.io/live/repository/packages/Digitalroot-Heightmap_Unlimited_Remake-1.4.0.zip
 wget https://gcdn.thunderstore.io/live/repository/packages/JereKuusela-Vanity-1.8.0.zip
 wget https://gcdn.thunderstore.io/live/repository/packages/Azumatt-AzuAutoStore-2.1.6.zip
 wget https://gcdn.thunderstore.io/live/repository/packages/Azumatt-AzuCraftyBoxes-1.2.9.zip
-wget https://gcdn.thunderstore.io/live/repository/packages/Azumatt-AzuContainerSizes-1.0.2.zip
+wget https://gcdn.thunderstore.io/live/repository/packages/Azumatt-AzuContainerSizes-1.0.3.zip
 wget https://gcdn.thunderstore.io/live/repository/packages/MSchmoecker-MultiUserChest-0.5.10.zip
 
