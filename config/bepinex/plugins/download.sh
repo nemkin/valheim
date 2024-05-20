e54ea7fb022af06d742fed2c2f7340ddc7095a15
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2024-05-20 13:11:41 +0100

    Add

--- config/bepinex/plugins/download.sh
+++ config/bepinex/plugins/download.sh
@@ -10,7 +10,6 @@ wget https://gcdn.thunderstore.io/live/repository/packages/clevel-BeastsOfBurden
 wget https://gcdn.thunderstore.io/live/repository/packages/denikson-BepInExPack_Valheim-5.4.2202.zip
 wget https://gcdn.thunderstore.io/live/repository/packages/GemHunter1-CustomChestName-1.1.1.zip
 wget https://gcdn.thunderstore.io/live/repository/packages/Digitalroot-Heightmap_Unlimited_Remake-1.4.1.zip
-wget https://gcdn.thunderstore.io/live/repository/packages/KGvalheim-ItemDrawers-1.0.8.zip
 wget https://gcdn.thunderstore.io/live/repository/packages/ValheimModding-Jotunn-2.20.0.zip
 wget https://gcdn.thunderstore.io/live/repository/packages/MSchmoecker-MultiUserChest-0.5.10.zip
 wget https://gcdn.thunderstore.io/live/repository/packages/Nextek-SpeedyPaths-1.0.8.zip
